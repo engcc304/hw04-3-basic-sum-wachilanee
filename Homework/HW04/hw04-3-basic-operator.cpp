94d7343c26ecedbe04dc4afaff47bc52e456830d
wachilanee <wachilanee47@gmail.com>
2023-07-20 15:50:57 +0700

    123

--- Homework/HW04/hw04-3-basic-operator.cpp
+++ Homework/HW04/hw04-3-basic-operator.cpp
@@ -12,4 +12,12 @@
     Output:
         Answer = 6
 */
-#include <stdio.h>
+#include <stdio.h> 
+int main(){
+
+    int num1 = 0, num2 = 0, num3 = 0;
+    printf( "Please enter value (num1 num2 num 3) : " ) ;
+    scanf( "%d %d %d", &num1, &num2, &num3 ) ;
+    printf( "Answer = %d", num1+num2+num3 ) ;
+    return 0 ;  
+}
